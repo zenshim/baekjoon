85b74c759893c494bca57809038f00b82c9e0e75
zenshim <zenshim24@uos.ac.kr>
2025-10-10 15:49:08 +0900

    10.10

---
+++ 수뒤집기.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/*1234->4321·Î Ãâ·Â*/
+int result=0;
+
+int reverse(int n) {
+    int reversed = 0;
+    while (n!=0) {
+        reversed = reversed*10 + n%10;
+        n = n/10;
+    }
+    return reversed;
+}
+
+void test(int added) {
+    int reversed = reverse(added);
+
+    if (added == reversed) 
+        printf("YES\n");
+    else    
+        printf("NO\n");
+}
+
+int main() {
+    int t, n, rn;
+    scanf("%d", &t);
+    for (int i=0;i<t;i++) {
+        scanf("%d", &n);
+        rn = reverse(n);
+        test(n+rn);
+    }
+
+    return 0;
+}
