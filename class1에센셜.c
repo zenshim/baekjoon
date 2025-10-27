6916229d052fd6bb0298085429a2f0bdf9af5222
zenshim <zenshim24@uos.ac.kr>
2025-10-27 23:47:55 +0900

    10.27

---
+++ class1에센셜.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/*별 찍기-1*/
+void starprint() {
+    int n;
+    scanf("%d", &n);
+    for (int i=1;i<=n;i++) {
+        for (int j=0;j<i;j++)
+            printf("*");
+        printf("\n");
+    }
+}
+/*Hello World*/
+void helloworld() {
+    printf("Hello World!");
+}
+/*구구단*/
+void gugu() {
+    int n;
+    scanf("%d", &n);
+    for (int i=1;i<=9;i++)
+        printf("%d * %d = %d\n", n, i, n*i);
+}
+
+
+int main() {
+    starprint();
+    helloworld();
+    gugu();
+
+    return 0;
+}
